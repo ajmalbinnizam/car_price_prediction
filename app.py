d48f389cf727ad0399950085c26dc1675ed63d58
ajmalbinnizam <ajmalnizam369@gmail.com>
2021-02-06 07:22:45 +0530

    new page

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -43,9 +44,9 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('predict.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {} lakhs".format(output))
+            return render_template('predict.html',prediction_text="You Can Sell The Car at {} lakhs".format(output))
     else:
         return render_template('index.html')
 

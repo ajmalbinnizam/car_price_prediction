921ac68899d4d22bd1224cac9c21e96671734455
ajmalbinnizam <ajmalnizam369@gmail.com>
2021-02-06 20:11:56 +0530

    three models compared

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+model = pickle.load(open('knn_model.pkl', 'rb'))
 
 @app.route('/',methods=['GET'])
 def Home():
